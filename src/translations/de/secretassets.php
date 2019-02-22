ee894907629dceb5eb54ffd3240d9621a2abf17f
handplant <webworker.ag@gmail.com>
2019-02-22 11:03:43 +0100

    fix translations

--- src/translations/de/secretassets.php
+++ src/translations/de/secretassets.php
@@ -2,8 +2,8 @@
 
 // german translations
 return [
-    'Secret Assets plugin loaded' => 'Secret Assets plugin wurde geladen',
-    'volume-name' => 'Ordner mit Zugriffschutz',
+    'Secret Assets plugin loaded' => 'SecretAssets plugin wurde geladen',
+    'volume-name' => 'Lokaler Ordner mit Schutz',
     'file-path-info' => 'Der Pfad zum Verzeichnis des Volumens im Dateisystem. Ein absoluter Pfad ({ex1}) wird empfohlen. Das Verzeichnis sollte ausserhalb des web root Verzeichnis liegen oder gegen direkten Zugriff aus dem Web gesichert sein.',
     'plugin-info' => 'Aktivieren Sie öffentliche URLs für dieses Volume, damit Benutzer mit entsprechenden Berechtigungen über das Frontend auf Dateien zugreifen können. Alle Urls werden automatisch mit dem Präfix "/secret" versehen, damit die Berechtigungen beim Zugriff überprüft werden können.',
 ];
---
+++ src/translations/en/secretassets.php
@@ -0,0 +1,9 @@
+<?php
+
+// english translations
+return [
+    'Secret Assets plugin loaded' => 'Secret Assets plugin loaded',
+    'volume-name' => 'Local Folder (protected)',
+    'file-path-info' => 'The path to the volume’s directory on the file system. An absolute path ({ex1}) is recommended. It should be located outside of your web root or protected against direct web access.',
+    'plugin-info' => 'Enable public URLs for this volume so that users with appropriate permissions can access files through the frontend. All urls are automatically prefixed with "/secret" so that permissions can be checked on access.',
+];
